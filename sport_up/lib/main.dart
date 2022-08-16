8c495a4f7f1be46eb9f2496045569d3eb3396c54
Birsan Raul <birsanraul1@yahoo.com>
2022-08-16 21:37:52 +0300

    screenurile de signin si signup

--- sport_up/lib/main.dart
+++ sport_up/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import './UI/screens/home_screen.dart';
 import './UI/screens/add_event_screen.dart';
@@ -6,15 +9,20 @@ import './UI/screens/profile_screen.dart';
 import 'UI/screens/login/sign_in_screen.dart';
 import 'UI/screens/login/sign_up_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final CollectionReference _users =
+      FirebaseFirestore.instance.collection("users");
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: "/sign_in",
+      initialRoute: "/sign_up",
       routes: {
         "/": (context) => HomeScreen(),
         "/add_event": (context) => AddEventScreen(),
